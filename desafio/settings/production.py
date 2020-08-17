77f928f05f13a706a6128f55ae4c26f9bfec00c0
Antolina Gonzalez <antolina487@gmail.com>
2020-08-16 23:14:28 -0300

    feature: deploy heroku master

--- desafio/settings/production.py
+++ desafio/settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DEBUG = True
+ALLOWED_HOSTS - ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
