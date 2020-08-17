50388b565132110fb5bb1a18227aff86cf686b71
Antolina Gonzalez <antolina487@gmail.com>
2020-08-16 22:48:48 -0300

    feature: heroku deploy

--- desafio/settings.py
+++ desafio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^k(lx&l&#pm6e9vu3zxrv__!sr5m5vn@1v6z!0d=w3lgifcy=n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitemoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
